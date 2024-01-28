0c95d9db482bad588a74a44a152acd302fa0a5b6
Turtletnt5 <mdturtleman@gmail.com>
2024-01-28 16:50:20 -0500

    level 1 combined and ready

--- Assets/changeLevel.cs
+++ Assets/changeLevel.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class changeLevel : MonoBehaviour
 {
@@ -19,7 +20,7 @@ public class changeLevel : MonoBehaviour
     void OnTriggerEnter2D(Collider2D col)
     {
         Debug.Log("change scean");
-        
+        SceneManager.LoadScene("GameChaptersScene");
     }
     
 }
