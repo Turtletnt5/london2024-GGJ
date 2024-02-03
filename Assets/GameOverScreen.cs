f260c07a336a804ac4840e22541ebb2d3c9891c2
KyleGabiane <72885112+Kronos3458@users.noreply.github.com>
2024-02-03 13:25:59 -0500

    Adding a Game Over Screen for Level 2
    Update:
    - Adding a Game Over Screen for Level 2 after the player loses 3 lives.

---
+++ Assets/GameOverScreen.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverScreen : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Level2Scene");
+    }
+
+    public void ExitGame()
+    {
+        Debug.Log("MainMenuScene");
+        Application.Quit();
+    }
+}
